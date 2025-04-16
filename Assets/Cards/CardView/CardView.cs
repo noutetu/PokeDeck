61047e946d652bd960106d09d97289af3168ceac
Runaki <beyoburo@gmail.com>
2025-04-16 19:40:59 +0900

    背景を解像度に対応するように

--- Assets/Cards/CardView/CardView.cs
+++ Assets/Cards/CardView/CardView.cs
@@ -11,37 +11,8 @@ public class CardView : MonoBehaviour
 {
     [SerializeField] private RawImage cardImage;
 
-    void Start()
-    {
-        Debug.Log("カードビューの初期化");
-    }
-
     public void Setup(CardModel data)
     {
-        Debug.Log("カードデータをセットアップ: " + data.name);
-        if (cardImage == null)
-        {
-            Debug.LogError("❌ cardImageがnullだよ！");
-            return;
-        }
-        if (data.imageTexture == null)
-        {
-            Debug.LogError("❌ imageTextureがnullだよ！");
-            return;
-        }
-        if (data.imageTexture.width == 0 || data.imageTexture.height == 0)
-        {
-            Debug.LogError("❌ imageTextureのサイズが0だよ！");
-            return;
-        }
-        if (data.imageTexture.width > 0 && data.imageTexture.height > 0)
-        {
-            Debug.Log("カード画像のサイズ: " + data.imageTexture.width + "x" + data.imageTexture.height);
-        }
-        else
-        {
-            Debug.LogError("❌ imageTextureのサイズが不正だよ！");
-        }
         cardImage.texture = data.imageTexture;
     }
 }
